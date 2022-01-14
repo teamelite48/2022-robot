6a358bc7d20a3993f06f2c656faca5e8ae73f1b5
Matthew Jones <mtjones330@fastmail.com>
2022-01-13 19:25:34 -0500

    Add IntakeSubsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,19 +6,31 @@ package frc.robot;
 
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 
 
 public class RobotContainer {
   
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+  
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final Command intakeCommand = new RunCommand(() -> intakeSubsystem.intake(), intakeSubsystem); 
+  private final Command outtakeCommand = new RunCommand(() -> intakeSubsystem.outtake(), intakeSubsystem);
+  private final Command stopOuttakeCommand = new RunCommand(() -> intakeSubsystem.stop(), intakeSubsystem);
+
 
   public RobotContainer() {
+    intakeSubsystem.setDefaultCommand(intakeCommand);
+
     configureButtonBindings();
   }
 
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    
+  }
 
   public Command getAutonomousCommand() {
     return m_autoCommand;
