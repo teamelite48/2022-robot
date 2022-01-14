ce21def3ec3ccdb2739821ca566a4995f1348783
Matthew Jones <mtjones330@fastmail.com>
2022-01-13 19:25:34 -0500

    Add IntakeSubsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+  private final PWMSparkMax motor = new PWMSparkMax(0);
+    
+  public IntakeSubsystem() {}
+
+  @Override
+  public void periodic() {
+    
+  }
+  public void intake() {
+    motor.set(0.5);
+  }
+  
+  public void outtake() {
+    motor.set(-0.5);
+  }
+
+  public void stop() {
+      motor.set(0);
+  }
+}
