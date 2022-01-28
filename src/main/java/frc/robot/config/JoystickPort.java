2ca481c6620e748b7ff550699547821afadb04b8
Matthew Jones <mtjones330@fastmail.com>
2022-01-27 22:04:05 -0500

    Add shooter subsystem

--- src/main/java/frc/robot/config/JoystickPort.java
+++ src/main/java/frc/robot/config/JoystickPort.java
@@ -3,5 +3,5 @@ package frc.robot.config;
 public final class JoystickPort {
     public final static int LeftPilotJoystick = 0;
     public final static int RightPilotJoystick = 1;
-    public final static int CopilotGamepad = 3;
+    public final static int CopilotGamepad = 2;
 }
