f810c3c267b60e00105d256c0263b64a3c5e1ff2
Aidan Rohrer <aidanrhysrohrer1@gmail.com>
2022-02-01 18:42:12 -0500

    Cleaned up Elevator Escalator system

--- src/main/java/frc/robot/subsystems/EscalatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/EscalatorSubsystem.java
@@ -6,13 +6,12 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.config.EscalatorConfig;
 import frc.robot.config.PwmPort;
 
 public class EscalatorSubsystem extends SubsystemBase {
   
   private final PWMSparkMax motor = new PWMSparkMax(PwmPort.EscalatorMotor);
-
-  private final double motorSpeed = 0.5;
 
   public EscalatorSubsystem() {}
 
@@ -20,11 +19,16 @@ public class EscalatorSubsystem extends SubsystemBase {
   public void periodic() {
     
   }
-  public void forward(){
-    motor.set(motorSpeed);
-  }
-public void backwards(){
-  motor.set(-motorSpeed);
-}
 
+  public void in(){
+    motor.set(EscalatorConfig.MotorSpeed);
+  }
+
+  public void out(){
+    motor.set(-EscalatorConfig.MotorSpeed);
+  }
+
+  public void stop(){
+    motor.set(0);
+  }
 }
