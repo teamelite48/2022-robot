cd89e81235a1bfd01c3654fd13546a6375fb50cc
Nadia Petersen <investigationslena@gmail.com>
2022-02-05 14:38:06 -0500

    Bind Climber Subsystem Controls

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -9,6 +9,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.config.CanBusId;
 import frc.robot.config.PneumaticChannel;
@@ -22,31 +23,54 @@ public class ClimberSubsystem extends SubsystemBase {
   private final DoubleSolenoid rightArmSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, PneumaticChannel.RightArmForward, PneumaticChannel.RightArmReverse);
 
   private final double motorSpeed = 0.5;
+  private boolean isClimberEnabled = false;
 
   public ClimberSubsystem() {
-  
     leftArmSolenoid.set(Value.kReverse);
     rightArmSolenoid.set(Value.kReverse);
-
   }
 
   @Override
-  public void periodic() {}
+  public void periodic() {
+    SmartDashboard.putBoolean("Climber Enabled", isClimberEnabled);
+    SmartDashboard.putString("Left Arm Position", leftArmSolenoid.get().toString());
+    SmartDashboard.putString("Right Arm Position", rightArmSolenoid.get().toString());
+  }
 
+  public void enableClimber() {
+    isClimberEnabled = true;
+  }
 
+  public void toggleLeftArmPosition() {
+    if (isClimberEnabled == false){
+      return;
+    }
 
-  public void toggleLeftArm() {
     leftArmSolenoid.toggle();
   }
-  public void toggleRightArm() {
+
+  public void toggleRightArmPosition() {
+    if (isClimberEnabled == false){
+      return;
+    }
+    
     rightArmSolenoid.toggle();
   }
 
-  public void extendLeftArm() {
-    leftArmMotor.set(motorSpeed);
+  public void adjustArmLengths(double leftSpeed, double rightSpeed) {
+    if (isClimberEnabled == false) {
+      return;
+    }
+
+    leftArmPosition(leftSpeed);
+    rightArmPosition(rightSpeed);
   }
-  public void extendRightArm(){
-    rightArmMotor.set(motorSpeed);
+
+  public void leftArmPosition(double speed) {
+    leftArmMotor.set(speed);
   }
-  
+
+  public void rightArmPosition(double speed){
+    rightArmMotor.set(speed);
+  }
 }
