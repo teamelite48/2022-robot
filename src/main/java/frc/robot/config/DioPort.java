fd6547a968f1f56db12d994d75c297535d60d30c
Matthew Jones <mtjones330@fastmail.com>
2022-02-11 18:58:03 -0500

    Replace analog gyro with adis gyro

--- src/main/java/frc/robot/config/DioPort.java
+++ src/main/java/frc/robot/config/DioPort.java
@@ -1,8 +1,8 @@
 package frc.robot.config;
 
 public final class DioPort {
-    public static final int LeftEncoderChannelA = 0;
-    public static final int LeftEncoderChannelB = 1;
-    public static final int RightEncoderChannelA = 2;
-    public static final int RightEncoderChannelB = 3;
+    public static final int LeftEncoderChannelA = 2;
+    public static final int LeftEncoderChannelB = 3;
+    public static final int RightEncoderChannelA = 0;
+    public static final int RightEncoderChannelB = 1;
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,8 +4,9 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -34,9 +35,9 @@ public class DriveSubsystem extends SubsystemBase {
   private Encoder leftEncoder = new Encoder(DioPort.LeftEncoderChannelA, DioPort.LeftEncoderChannelB);
   private Encoder rightEncoder = new Encoder(DioPort.RightEncoderChannelA, DioPort.RightEncoderChannelB);
 
-  private AnalogGyro gyro = new AnalogGyro(1);
+  private ADIS16470_IMU gyro = new ADIS16470_IMU();
 
-  private DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(gyro.getRotation2d());
+  private DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(gyro.getAngle()));
   private Field2d field = new Field2d();
 
   private DriveSimulation simulation;
@@ -57,12 +58,13 @@ public class DriveSubsystem extends SubsystemBase {
   public void periodic() {
 
     odometry.update(
-      gyro.getRotation2d(),
+      Rotation2d.fromDegrees(gyro.getAngle()),
       leftEncoder.getDistance(),
       rightEncoder.getDistance()
     );
 
     field.setRobotPose(odometry.getPoseMeters());
+    SmartDashboard.putNumber("Gyro Angle", gyro.getAngle());
   }
 
   public void simulationPeriodic() {
