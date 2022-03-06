7632f52c22adc6543a568eec0c6da7caf8e9682c
Trevor <trev111490@gmail.com>
2022-03-06 11:48:16 -0500

    Change shooter and turret values

--- src/main/java/frc/robot/config/subsystems/ShooterConfig.java
+++ src/main/java/frc/robot/config/subsystems/ShooterConfig.java
@@ -2,16 +2,16 @@ package frc.robot.config.subsystems;
 
 public final class ShooterConfig {
 
-    public static final double lowRPM = 2000;
-    public static final double mediumRPM = 3000;
-    public static final double highRPM = 4000;
+    public static final double lowRPM = 2750;
+    public static final double mediumRPM = 3600;
+    public static final double highRPM = 4500;
 
     public static final double maxRPM = 4850;
 
     public static final boolean deflectorForwardValue = true;
     public static final boolean deflectorBackwardValue = false;
 
-    public static final double kP = 0.000133;
+    public static final double kP = 0.00015;
     public static final double kI = 0;
     public static final double kD = 0;
 }
--- src/main/java/frc/robot/config/subsystems/TurretConfig.java
+++ src/main/java/frc/robot/config/subsystems/TurretConfig.java
@@ -9,7 +9,7 @@ public class TurretConfig {
     public static final int encoderLimit = 6;
     public static final double nominalMotorRotationsPerSecond = 11000 / 60.0;
 
-    public static final double kP = 0.026;
+    public static final double kP = 0.04;
     public static final double kI = 0.0003;
     public static final double kD = 0.005;
 }
