95b50563e1ea7848036e7230ca0feec6c62dfe32
Matthew Jones <mtjones330@fastmail.com>
2022-03-07 23:51:49 -0500

    Invert ball sensor value

--- src/main/java/frc/robot/config/subsystems/ShooterFeedConfig.java
+++ src/main/java/frc/robot/config/subsystems/ShooterFeedConfig.java
@@ -5,6 +5,6 @@ public final class ShooterFeedConfig {
     public static final double upSpeed = -0.5;
     public static final double downSpeed = 0.5;
 
-    public static final boolean ballSensedValue = true;
+    public static final boolean ballSensedValue = false;
     public static final int ballCoolDownTimer = 500;
 }
