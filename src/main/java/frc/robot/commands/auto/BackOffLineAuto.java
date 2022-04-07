c6c6746ffa443d9ce1958e47f8090b63ba1176d6
Matthew Jones <mtjones330@fastmail.com>
2022-04-07 16:52:52 -0400

    Use medium shooter speed for back off line auto

--- src/main/java/frc/robot/commands/auto/BackOffLineAuto.java
+++ src/main/java/frc/robot/commands/auto/BackOffLineAuto.java
@@ -6,9 +6,7 @@ import frc.robot.RobotContainer;
 import frc.robot.commands.drive.FollowPath;
 import frc.robot.commands.drive.ResetOdometry;
 import frc.robot.commands.shooter.ShooterOff;
-import frc.robot.commands.shooter.ShooterOn;
 import frc.robot.commands.shooterfeed.ShooterFeedUp;
-import frc.robot.commands.sorter.SorterIn;
 import frc.robot.pathfollowing.PathType;
 
 public class BackOffLineAuto extends SequentialCommandGroup {
@@ -17,11 +15,9 @@ public class BackOffLineAuto extends SequentialCommandGroup {
         addCommands(
             new ResetOdometry(10.5, 3.45, 180),
             new InstantCommand(RobotContainer.shooterSubsystem::setMediumSpeed),
-            new ShooterOn(),
-            new InstantCommand(RobotContainer.shooterSubsystem::turnRangeBasedRPMOn),
+            new InstantCommand(RobotContainer.shooterSubsystem::shooterOn),
             new FollowPath(PathType.BackOffLine),
             new InstantCommand(RobotContainer.turretSubsystem::turnAutoAimOn),
-            new SorterIn(),
             new ShooterFeedUp().withTimeout(2),
             new ShooterOff()
         );
