006fef2d9599c71e0b001dea2951d0eaf6fe3c25
Matthew Jones <mtjones330@fastmail.com>
2022-04-07 18:37:44 -0400

    Added retract to two ball auto

--- src/main/java/frc/robot/commands/auto/TwoBallAuto.java
+++ src/main/java/frc/robot/commands/auto/TwoBallAuto.java
@@ -7,6 +7,7 @@ import frc.robot.RobotContainer;
 import frc.robot.commands.drive.FollowPath;
 import frc.robot.commands.drive.ResetOdometry;
 import frc.robot.commands.intake.AutoIntake;
+import frc.robot.commands.intake.RetractIntake;
 import frc.robot.commands.shooter.ShooterOff;
 import frc.robot.commands.shooterfeed.ShooterFeedUp;
 import frc.robot.commands.sorter.SorterIn;
@@ -25,6 +26,7 @@ public class TwoBallAuto extends SequentialCommandGroup {
             new FollowPath(PathType.TwoBall1),
             new InstantCommand(RobotContainer.turretSubsystem::turnAutoAimOn),
             new WaitCommand(0.5),
+            new RetractIntake(),
             new ShooterFeedUp().withTimeout(3),
             new ShooterOff()
         );
